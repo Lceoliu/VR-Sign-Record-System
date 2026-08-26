5ecc93becb10ab174e63480b23a40ba85af36fd8
woshica <chengang@shanghaitech.edu.cn>
2026-08-27 00:30:27 +0800

    test: specify Quest-local startup recovery

---
+++ signvr_unity/Assets/Tests/EditMode/Interaction/CaptureHost/StandaloneLocalRunRecoveryTests.cs
@@ -0,0 +1,86 @@
+using System;
+using System.Reflection;
+using System.Runtime.ExceptionServices;
+using NUnit.Framework;
+
+namespace SignVR.Interaction.Editor.Tests
+{
+    public sealed class StandaloneLocalRunRecoveryTests
+    {
+        private const string DriverTypeName =
+            "SignVR.Interaction.CaptureHost." +
+            "StandaloneLocalRunRecoveryTestDriver, Assembly-CSharp";
+
+        [Test]
+        public void SealedCompletedRunIsLocallyCompleteWithoutHostAck()
+        {
+            Invoke(nameof(SealedCompletedRunIsLocallyCompleteWithoutHostAck));
+        }
+
+        [Test]
+        public void SealedAbortedRunIsLocallyCompleteWithoutHostAck()
+        {
+            Invoke(nameof(SealedAbortedRunIsLocallyCompleteWithoutHostAck));
+        }
+
+        [Test]
+        public void StartupRecoverySealsOnlyUnsealedRunAndPreservesValidRows()
+        {
+            Invoke(nameof(StartupRecoverySealsOnlyUnsealedRunAndPreservesValidRows));
+        }
+
+        [Test]
+        public void StartupRecoveryIsIdempotent()
+        {
+            Invoke(nameof(StartupRecoveryIsIdempotent));
+        }
+
+        [Test]
+        public void ManifestIdentityMismatchFailsClosedAndRetainsEvidence()
+        {
+            Invoke(nameof(ManifestIdentityMismatchFailsClosedAndRetainsEvidence));
+        }
+
+        [Test]
+        public void CorruptManifestFailsClosedAndRetainsEvidence()
+        {
+            Invoke(nameof(CorruptManifestFailsClosedAndRetainsEvidence));
+        }
+
+        [Test]
+        public void RecoveryWriteFailureFailsClosedAndRetainsEvidence()
+        {
+            Invoke(nameof(RecoveryWriteFailureFailsClosedAndRetainsEvidence));
+        }
+
+        [Test]
+        public void RecoveryCoordinatorPublishesPollableStartupState()
+        {
+            Invoke(nameof(RecoveryCoordinatorPublishesPollableStartupState));
+        }
+
+        private static void Invoke(string methodName)
+        {
+            Type driver = Type.GetType(DriverTypeName, throwOnError: true);
+            MethodInfo method = driver.GetMethod(
+                methodName,
+                BindingFlags.Public | BindingFlags.Static
+            );
+            Assert.That(
+                method,
+                Is.Not.Null,
+                "Missing standalone recovery scenario " + methodName
+            );
+            try
+            {
+                method.Invoke(null, null);
+            }
+            catch (TargetInvocationException exception)
+                when (exception.InnerException != null)
+            {
+                ExceptionDispatchInfo.Capture(exception.InnerException).Throw();
+                throw;
+            }
+        }
+    }
+}
